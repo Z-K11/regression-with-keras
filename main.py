05e4016cc701a3268fe2b3126be4a67638a84016
Z-K11 <zulqarnainbazidkhail@gmail.com>
2025-02-11 10:34:20 +0500

    split target and prediction variables

--- main.py
+++ main.py
@@ -8,3 +8,11 @@ print(concrete_data.isnull().sum())
 '''.isnull() returns a data frame that is the same shape as conrete_data but has true for any NaN value and false otherwise meaning that 
 it is 1 for missing value and 0 for value .sum() adds these values if the sum is 0 we have no missing value if the sum is n then we have 
 n missing values '''
+conc_data_columns=concrete_data.columns
+'''returns the column names of the data set as pandas index object '''
+predictors = concrete_data[conc_data_columns[conc_data_columns !='Strength']]
+'''by default pandas performed row based filtering see google docs (Deep Learning/page = Pandas Filtering for explanation)'''
+target = concrete_data['Strength']
+'''selects the 'Strength' column and returns a pandas series  which is a 1d array'''
+print(target.head())
+print(predictors.head())
